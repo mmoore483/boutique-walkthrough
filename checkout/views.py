4bd638c129525479fa175d18f6cceb79cd0e8cb2
mmoore483 <mary.e.moore@hotmail.co.uk>
2022-04-10 13:06:36 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51KmybSEuFiulGcYzwy8QRek5gR43yERIuTgHyYlKIAV8UA5KALXayAyRgY1eqTTCMzR17He0Q5GJlZCEOh0U8bFb002rvRpwi1',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
